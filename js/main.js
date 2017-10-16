2fc649af6a3a83007bb17b00bfeb871de9dbff31
Tomohito Yamazaki <t07840ty@gmail.com>
2017-10-16 13:43:28 +0900

    "Add enemy and time label"

--- js/main.js
+++ js/main.js
@@ -20,17 +20,69 @@ window.onload = function(){
 
     // 
     bear.addEventListener('enterframe', function(){
-      if (core.input.left) this.x -= 5;
-      if (core.input.right) this.x += 5;
-      if (core.input.up) this.y -= 5;
-      if (core.input.down) this.y += 5;
+      if (core.input.right) this.x = this.x + 5;
+      if (core.input.left) this.x = this.x - 5;
+      if (core.input.up) this.y = this.y - 5;
+      if (core.input.down) this.y = this.y + 5;
+      // intersect
+      if (this.intersect(enemy)) {
+        // label.text = 'hit!';
+      }
+      // within
+      if (this.within(enemy)){
+        // 
+        core.stop();
+      }
     });
+
+
     // 
-    core.rootScene.on('touchstart',function(e){
-      bear.x = e.x;
-      bear.y = e.y;
+    let enemy = new Sprite(32,32);
+    // 
+    enemy.image = core.assets['../img/chara1.png'];
+    // 
+    enemy.x = 400;
+    // 
+    enemy.y = 400;
+    //
+    enemy.frame = 5;
+    //
+    enemy.addEventListener('enterframe', function(){
+      // x成分とy成分の比に分ける
+      let cos_theta = (bear.x - this.x) / Math.sqrt(Math.pow(bear.x - this.x,2)+Math.pow(bear.x - this.x,2));
+      let sin_theta = (bear.y - this.y) / Math.sqrt(Math.pow(bear.y - this.y,2)+Math.pow(bear.y - this.y,2));
+      // スピードを決める
+      let speed = 4.5;
+
+      // x座標、y座標と進む値を決める
+      this.x = this.x + speed*cos_theta;
+      this.y = this.y + speed*sin_theta;
     });
+
+    let gameOverScene = new Scene();
+    gameOverScene.backgroundColor = 'black';
+
+    // 
+    let label = new Label();
+    // 
+    label.x = 400;
+    // 
+    label.y = 5;
+    // 
+    label.color = 'red';
+    // 
+    label.font = '14px "Arial"';
+    // 
+    label.text = '0';
+    // 
+    label.on('enterframe', function(){
+      label.text = (core.frame / core.fps).toFixed(1);
+    })
     core.rootScene.addChild(bear);
+    // 
+    core.rootScene.addChild(label);
+    // 
+    core.rootScene.addChild(enemy);
   }
   // 処理を開始する
   core.start();
